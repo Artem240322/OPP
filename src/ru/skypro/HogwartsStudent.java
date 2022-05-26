0013e2850ff3d1841551eaeea22e6e7992059199
ArtemCH240322 <sun2383@mail.ru>
2022-05-26 15:12:11 +0300

    Первый комит OPP. Хогвартс - добавляю гетеры и сетеры.

---
+++ src/ru/skypro/HogwartsStudent.java
@@ -0,0 +1,44 @@
+package ru.skypro;
+
+public class HogwartsStudent {
+    private String name;
+    private int magic;
+    private int transgression;
+
+    public HogwartsStudent(String name, int magic, int transgression) {
+        this.name = name;
+        this.magic = magic;
+        this.transgression = transgression;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMagic() {
+        return magic;
+    }
+
+    public void setMagic(int magic) {
+        this.magic = magic;
+    }
+
+    public int getTransgression() {
+        return transgression;
+    }
+
+    public void setTransgression(int transgression) {
+        this.transgression = transgression;
+    }
+
+
+    @Override
+    public String toString() {
+        return String.format("Студент: %S; Сила магии: %D; Сила трансгрессии: %G", name, magic, transgression);
+    }
+
+}
