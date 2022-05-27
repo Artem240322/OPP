764a88b98c472412f8e1c23acfe14f853a763e05
ArtemCH240322 <sun2383@mail.ru>
2022-05-27 11:41:13 +0300

    Первый комит OPP. Хогвартс - добавляю гетеры и сетеры. Грифендорф. ХуффлепуФФ. Ревенслав. Слизерен. Распечатка. Сравнение ++.

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,31 @@
 package ru.skypro;
 
+import java.util.Random;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    private static final Random RANDOM = new Random();
+
+    public static void main (String[] args) {
+
+        HogwartsStudent harry;
+        harry = GriffindorSudent("Гарри Потер", generate(), generate(), generate(), generate(), generate());
+        HogwartsStudent ron = GriffindorSudent("Рон Уизли", generate(), generate(), generate(), generate(), generate());
+        HogwartsStudent luna = RavenclawStudent("Палувна Лавгуд", generate(), generate(), generate(), generate(), generate());
+        HogwartsStudent draco = SlytherinStudent("Драко Малфлой", generate(), generate(), generate(), generate(), generate());
+        HogwartsStudent sedric = HufflepuffStudent("Седрик Диггори", generate(), generate(), generate(), generate(), generate());
+
+        System.out.println(harry);
+        System.out.println(ron);
+        System.out.println(luna);
+        System.out.println(draco);
+        System.out.println(sedric);
+
+        harry.compareTo(draco);
+        harry.compareTo(ron);
+    }
+
+    private static int generat() {
+        return RANDOM.nextInt(100);
     }
 }
