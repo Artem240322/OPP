3f1282aa04988830741c4e4d8c018eaa44bc3fde
ArtemCH240322 <sun2383@mail.ru>
2022-05-27 13:33:31 +0300

    Первый комит OPP. Хогвартс - добавляю гетеры и сетеры. Грифендорф. ХуффлепуФФ. Ревенслав. Слизерен. Распечатка. Сравнение ++. ХЗ что происходит int добавил в генератор. код работает. Сравнение студентов - факультетов. Правки. Правки.

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -8,11 +8,11 @@ public class Main {
 
     public static void main (String[] args) {
 
-        HogwartsStudent harry = new GriffindorSudent("Гарри Потер", generate(), generate(), generate(), generate(), generate());
-        HogwartsStudent ron = new GriffindorSudent("Рон Уизли", generate(), generate(), generate(), generate(), generate());
-        HogwartsStudent luna = new RavenclawStudent("Палувна Лавгуд",generate(), generate(), generate(), generate(), generate());
-        HogwartsStudent draco = new SlytherinStudent("Драко Малфлой", generate(), generate(), generate(), generate(), generate(), generate(), generate());
-        HogwartsStudent sedric = new HufflepuffStudent("Седрик Диггори", generate(), generate(), generate(), generate(), generate());
+        GriffindorSudent harry = new GriffindorSudent("Гарри Потер", generate(), generate(), generate(), generate(), generate());
+        GriffindorSudent ron = new GriffindorSudent("Рон Уизли", generate(), generate(), generate(), generate(), generate());
+        RavenclawStudent luna = new RavenclawStudent("Палувна Лавгуд",generate(), generate(), generate(), generate(), generate());
+        SlytherinStudent draco = new SlytherinStudent("Драко Малфлой", generate(), generate(), generate(), generate(), generate(), generate(), generate());
+        HufflepuffStudent sedric = new HufflepuffStudent("Седрик Диггори", generate(), generate(), generate(), generate(), generate());
 
         System.out.println(harry);
         System.out.println(ron);
@@ -21,7 +21,7 @@ public class Main {
         System.out.println(sedric);
 
         harry.compareHogwarts(draco);
-        harry.compareHogwarts(ron);
+        harry.compareGriffindor(ron);
     }
 
     private static int generate() {
